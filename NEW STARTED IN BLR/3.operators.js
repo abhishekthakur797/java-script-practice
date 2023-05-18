2b51c8f42e1ba3e5e0c9783c206eeae9c66cd080
Abhishek <officialabhishekthakur@gmail.com>
2023-05-18 23:48:04 +0530

    3.operator.js file has been added

---
+++ NEW STARTED IN BLR/3.operators.js
@@ -0,0 +1,13 @@
+//! Primitive Data Type
+//? String, Number, booleans, undefined, null, BigInt, Symbol
+
+let age = 24;
+//*typeof is a function which tells which type of data is used
+console.log(typeof age);
+console.log(typeof false);
+//* in typeof function if you (typeof, variable) it prints with the value
+console.log(typeof age, age);
+var king = "king kohli";
+console.log(typeof king, king);
+
+//! convert number to string
