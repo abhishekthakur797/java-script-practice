d5d48e61d37e2688b12aa2edab0143be7ed389ae
Abhishek <officialabhishekthakur@gmail.com>
2023-05-28 00:27:01 +0530

    new files were added

--- NEW HARSHIT_VASHISTH/4.string_concatenation.js
+++ NEW HARSHIT_VASHISTH/4.string_concatenation.js
@@ -11,7 +11,7 @@ console.log(typeof result2, result2);
 let myAge = 24;
 let myName = "Abhishek";
 console.log("my name is " + myName + " and my age is " + myAge);
-console.log(`my name is ${myName} and my ages is ${myAge}`); //backticks
+console.log(`my name is ${myName} and my age is ${myAge}`); //backticks
 console.log(
   `my name is ${myName} and my age is ${myAge} and marks in maths is ${myNo1} and in computer science is ${myNo2}`
 );
