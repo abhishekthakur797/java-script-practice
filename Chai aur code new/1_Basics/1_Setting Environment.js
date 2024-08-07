f406d70d966706815ec9a538a912777839ba5268
Abhishek <officialabhishekthakur@gmail.com>
2024-08-07 14:24:51 +0530

    100 days code challenge 1st day commit

---
+++ Chai aur code new/1_Basics/1_Setting Environment.js
@@ -0,0 +1,12 @@
+//* VIDEO NO 2: Setting environment in local machine.
+
+// JavaScript was originally run on the browser by adding a script file to HTML.
+// Now, JavaScript can also run standalone on the server side, similar to Python or Java.
+// JavaScript is used in backend development and mobile application development as well.
+// To run JavaScript standalone, you need to install Node.js or Deno.js.
+// Both Node.js and Deno.js were created by Ryan Dahl.
+// Deno.js also supports running TypeScript code.
+// To check if Node.js is installed on your PC, open the terminal and type: node -v
+// To run a JavaScript file using Node.js, type: node file.js
+
+console.log("Welcome to JavaScript");
